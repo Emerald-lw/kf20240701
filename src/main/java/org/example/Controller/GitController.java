256765ce3597f217f5a9ae5b39c30e1fe51e455b
Emerald <898954294@com.qq>
2024-07-02 09:04:23 +0800

    创建了一个Controller
    Signed-off-by: Emerald <898954294@com.qq>

--- src/main/java/org/example/Controller/GitController.java
+++ src/main/java/org/example/Controller/GitController.java
@@ -1,4 +1,18 @@
 package org.example.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 接口类
+ */
+
+@RestController
+@RequestMapping
 public class GitController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 }
